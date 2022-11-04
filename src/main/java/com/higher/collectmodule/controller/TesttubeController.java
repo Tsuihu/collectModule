b83bb01fe64a74fc4afa9c761c3307f82b290890
Xioajiuwo <468739212@qq.com>
2022-11-04 18:34:59 +0800

    试管的开封操作,以及查询此箱内所有管的状态和管管编码

---
+++ src/main/java/com/higher/collectmodule/controller/TesttubeController.java
@@ -0,0 +1,61 @@
+package com.higher.collectmodule.controller;
+
+import com.higher.collectmodule.exception.BusinessException;
+import com.higher.collectmodule.pojo.Testtube;
+import com.higher.collectmodule.service.TesttubeService;
+import com.higher.collectmodule.util.ResultCodeEnum;
+import com.higher.collectmodule.util.ResultModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.List;
+
+@RestController
+@RequestMapping("/testtube")
+public class TesttubeController {
+    @Autowired
+    TesttubeService testtubeService;
+
+    /**
+     *查询全部管的信息，包含管编号和状态,需要前端返回开箱时的boxId
+     * @return
+     */
+    @PostMapping("/getAllTube.do")
+    public ResultModel<List<Testtube>> getAllTube(@RequestBody Testtube testtube){
+        List<Testtube> testtubes= testtubeService.getAllTube(testtube.getBoxId());
+
+        return new ResultModel<>(ResultCodeEnum.SUCCESS, testtubes, "");
+    }
+
+    /**
+     * 开管，，需要前端返回开箱的boxId,以及输入的管编码testtubeCode，和选择采集类型的1，10或者20(collectType)
+     * @param testtube
+     * @return
+     * @throws BusinessException
+     */
+    @PostMapping("/addTube.do")
+    public ResultModel<Testtube> addTube(@RequestBody Testtube testtube) throws BusinessException {
+        testtube.setStatus(0);
+        testtube.setOpenTime(new Date());
+        testtube.setTestResult("0");
+        testtubeService.addTube(testtube);
+        return new ResultModel<Testtube>(ResultCodeEnum.SUCCESS, testtube, "");
+    }
+
+    /**
+     *封管，需要返回前端开管时的testtubeId
+     * @param testtube
+     * @return
+     */
+    @PostMapping("/closeTube.do")
+    public ResultModel<Testtube> closeTube(@RequestBody Testtube testtube){
+        testtube.setStatus(1);
+        testtube.setCloseTime(new Date());
+        testtubeService.closeTube(testtube.getStatus(),testtube.getCloseTime(),testtube.getTesttubeId());
+        return new ResultModel<>(ResultCodeEnum.SUCCESS, testtube, "");
+    }
+}
---
+++ src/main/java/com/higher/collectmodule/service/impl/TesttubeServiceImpl.java
@@ -0,0 +1,49 @@
+package com.higher.collectmodule.service.impl;
+
+import com.higher.collectmodule.dao.TesttubeDao;
+import com.higher.collectmodule.exception.BusinessException;
+import com.higher.collectmodule.pojo.Testtube;
+import com.higher.collectmodule.service.TesttubeService;
+import com.higher.collectmodule.util.ResultCodeEnum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class TesttubeServiceImpl implements TesttubeService {
+    @Autowired
+    TesttubeDao testtubeDao;
+//  查询箱下面的所有管的编码和状态,需要传递开箱时的boxId
+    @Override
+    public List<Testtube> getAllTube(Integer boxId) {
+        List<Testtube> allTube = testtubeDao.getAllTube(boxId);
+        return allTube;
+    }
+
+    /**
+     * 开管,需要传递开箱时的boxId,输入的开管编码testtubeCode,和选择采集类型的1，10或者20(collectType)
+     * @param testtube
+     * @throws BusinessException
+     */
+    @Override
+    public void addTube(Testtube testtube) throws BusinessException {
+//        进行判断,看看试管编码是否重复
+        if (testtubeDao.getOne(testtube.getTesttubeCode())>0){
+            throw new BusinessException("编号已存在", ResultCodeEnum.LOGIN_ERROR);
+        }
+        testtubeDao.addTube(testtube);
+    }
+
+    /**
+     * 封管,需要传递开管时的testtubeId
+     * @param status
+     * @param closeTime
+     * @param testtubeId
+     */
+    @Override
+    public void closeTube(Integer status, Date closeTime,int testtubeId) {
+        testtubeDao.closeTube(status,closeTime,testtubeId);
+    }
+}
